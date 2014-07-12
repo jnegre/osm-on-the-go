4a535f3830daeca8c1ecdec97e116338ef86178a
Jerome Negre <jerome@jnegre.org>
2014-07-12 21:05:12 +0200

    Adding BANO and NO_NAME overlays from OSM-FR

--- app/src/main/java/org/jnegre/android/osmonthego/activity/MapActivity.java
+++ app/src/main/java/org/jnegre/android/osmonthego/activity/MapActivity.java
@@ -5,6 +5,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -21,9 +22,12 @@ import org.jnegre.android.osmonthego.osmdroid.ControlOverlay;
 import org.jnegre.android.osmonthego.osmdroid.ExtraTileSourceFactory;
 import org.jnegre.android.osmonthego.provider.SurveyProviderMetaData;
 import org.osmdroid.api.IGeoPoint;
+import org.osmdroid.tileprovider.MapTileProviderBasic;
+import org.osmdroid.tileprovider.tilesource.OnlineTileSourceBase;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.TilesOverlay;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
@@ -35,11 +39,15 @@ public class MapActivity extends Activity {
 	private final static String PREF_ZOOM_LEVEL = "ZOOM_LEVEL";
 	private final static String PREF_SCROLL_X = "SCROLL_X";
 	private final static String PREF_SCROLL_Y = "SCROLL_Y";
+	private final static String PREF_OVERLAY_BANO_ENABLED = "OVERLAY_BANO_ENABLED";
+	private final static String PREF_OVERLAY_NO_NAME_ENABLED = "OVERLAY_NO_NAME_ENABLED";
 
 	private final static int REQUEST_CODE_NEW_ADDRESS = 1;
 
 
 	private MapView mapView;
+	private TilesOverlay banoOverlay;
+	private TilesOverlay noNameOverlay;
 	private MyLocationNewOverlay myLocationOverlay;
 
 	@Override
@@ -52,45 +60,49 @@ public class MapActivity extends Activity {
 		final DisplayMetrics dm = context.getResources().getDisplayMetrics();
 
 		mapView = (MapView) findViewById(R.id.mapview);
-
-		mapView.setTileSource(ExtraTileSourceFactory.OSM_FR);
-		//mapView.setTileSource(TileSourceFactory.MAPNIK);
-
-		//mapView.getOverlayManager().add(new TilesOverlay(new MapTileProviderBasic(context, ExtraTileSourceFactory.BANO), context));
-		//mapView.getOverlayManager().add(new TilesOverlay(new MapTileProviderBasic(context, ExtraTileSourceFactory.NO_NAME), context));
-
 		mapView.setMultiTouchControls(true);
 
-		Log.d(TAG, "restoring saved bounding box");
-		SharedPreferences pref = getPreferences(MODE_PRIVATE);
-		mapView.getController().setZoom(pref.getInt(PREF_ZOOM_LEVEL, 1));
-		mapView.scrollTo(pref.getInt(PREF_SCROLL_X, 0), pref.getInt(PREF_SCROLL_Y, 0));
-
+		mapView.setTileSource(ExtraTileSourceFactory.OSM_FR);
+		banoOverlay = addOverlay(ExtraTileSourceFactory.BANO);
+		noNameOverlay = addOverlay(ExtraTileSourceFactory.NO_NAME);
 
 		ScaleBarOverlay scaleBarOverlay = new ScaleBarOverlay(context);
 		scaleBarOverlay.setCentred(true);
 		scaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
 
-		myLocationOverlay = new MyLocationNewOverlay(context, new GpsMyLocationProvider(context),
-				mapView);
+		myLocationOverlay = new MyLocationNewOverlay(context, new GpsMyLocationProvider(context), mapView);
 
-		mapView.getOverlays().add(scaleBarOverlay);
 		mapView.getOverlays().add(this.myLocationOverlay);
-
 		mapView.getOverlays().add(new AddressOverlay(context));
-
+		mapView.getOverlays().add(scaleBarOverlay);
 		mapView.getOverlays().add(new ControlOverlay(context)); //must be the last one
 
+		Log.d(TAG, "restoring saved state");
+		SharedPreferences pref = getPreferences(MODE_PRIVATE);
+		mapView.getController().setZoom(pref.getInt(PREF_ZOOM_LEVEL, 1));
+		mapView.scrollTo(pref.getInt(PREF_SCROLL_X, 0), pref.getInt(PREF_SCROLL_Y, 0));
+		banoOverlay.setEnabled(pref.getBoolean(PREF_OVERLAY_BANO_ENABLED, false));
+		noNameOverlay.setEnabled(pref.getBoolean(PREF_OVERLAY_NO_NAME_ENABLED, false));
+	}
+
+	private TilesOverlay addOverlay(OnlineTileSourceBase source) {
+		Context context = this.getApplicationContext();
+		TilesOverlay tilesOverlay = new TilesOverlay(new MapTileProviderBasic(context, source), context);
+		tilesOverlay.setLoadingBackgroundColor(Color.argb(20,128,0,0));
+		mapView.getOverlayManager().add(tilesOverlay);
+		return tilesOverlay;
 	}
 
 	@Override
 	protected void onDestroy() {
-		Log.d(TAG, "saving bounding box");
+		Log.d(TAG, "saving state");
 		SharedPreferences pref = getPreferences(MODE_PRIVATE);
 		pref.edit()
 				.putInt(PREF_SCROLL_X, mapView.getScrollX())
 				.putInt(PREF_SCROLL_Y, mapView.getScrollY())
 				.putInt(PREF_ZOOM_LEVEL, mapView.getZoomLevel())
+				.putBoolean(PREF_OVERLAY_BANO_ENABLED, banoOverlay.isEnabled())
+				.putBoolean(PREF_OVERLAY_NO_NAME_ENABLED, noNameOverlay.isEnabled())
 				.commit();
 
 		super.onDestroy();
@@ -111,6 +123,9 @@ public class MapActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.map, menu);
+		SharedPreferences pref = getPreferences(MODE_PRIVATE);
+		menu.findItem(R.id.action_show_layer_bano).setChecked(pref.getBoolean(PREF_OVERLAY_BANO_ENABLED, false));
+		menu.findItem(R.id.action_show_layer_noname).setChecked(pref.getBoolean(PREF_OVERLAY_NO_NAME_ENABLED, false));
 		return true;
 	}
 
@@ -136,6 +151,16 @@ public class MapActivity extends Activity {
 				if (myLocation != null) {
 					mapView.getController().animateTo(myLocation);
 				}
+				return true;
+			case R.id.action_show_layer_bano:
+				item.setChecked(!item.isChecked());
+				banoOverlay.setEnabled(item.isChecked());
+				mapView.invalidate();
+				return true;
+			case R.id.action_show_layer_noname:
+				item.setChecked(!item.isChecked());
+				noNameOverlay.setEnabled(item.isChecked());
+				mapView.invalidate();
 				return true;
 			default:
 				return super.onOptionsItemSelected(item);
